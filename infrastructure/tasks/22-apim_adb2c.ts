d8921ce5e1572d56c6501df7bb1284ea018d88e5
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-11-28 15:54:49 +0100

    fix: active directory

--- infrastructure/tasks/22-apim_adb2c.ts
+++ infrastructure/tasks/22-apim_adb2c.ts
@@ -1,2 +1,10 @@
+// Add signin-signup policy to ADB2C tenant
+
+// Create an App in ADB2C tenant for the developer-portal:
+//    name: app-developer-portal-auth
+//    returnurl: https://${config.azurerm_apim}.portal.azure-api.net/signin-aad
+
+// Get CLIENT_ID and CLIENT_SECRET
+
 // Configure ADB2C authentication for API management
 // apiClient.identityProvider.createOrUpdate()
---
+++ infrastructure/tasks/30-onboarding_apiuser.ts
@@ -0,0 +1,11 @@
+// Create an App in ADB2C tenant for the onboarding application
+// Get CLIENT_ID and CLIENT_SECRET
+
+// Create an API management user (with script from onboarding)
+// Get API_KEY of the APIm user
+
+// Set CLIENT_ID, CLIENT_SECRET, API_KEY in the onboarding application settings
+
+// Activate MSI for onboarding web application
+
+// Add the onboarding application to the API management IAM as contributor
