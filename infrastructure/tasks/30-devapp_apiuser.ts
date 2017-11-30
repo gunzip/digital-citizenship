f642bede52634e78494721f5ac84469449e6b23d
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-11-30 18:29:40 +0100

    fix: apim user

--- infrastructure/tasks/30-devapp_apiuser.ts
+++ infrastructure/tasks/30-devapp_apiuser.ts
@@ -1,15 +1,99 @@
-/**
- * Create a Digital Citizenship API user running.
- * 
- * This is needed to const the Web application call the Digital Citizenship API,
- * to create a 'Service' and a 'Profile' tied to the developer portal accounts.
- */
+// tslint:disable:no-console
+// tslint:disable:no-any
+
 // Create an API management user (with script from onboarding)
 // Get API_KEY of the APIm user
-
-// Set CLIENT_ID, CLIENT_SECRET (from ADB2C app environment variables)
 // set API_KEY in the onboarding application settings
 // ADMIN_API_KEY=xxxxxxxxxxx
 
-// Manual: Activate MSI for onboarding web application
-// Manual: Add the onboarding application to the API management IAM as contributor
+/**
+ * This is a command line script that you can run to create
+ * a new API management user that has the rights to 
+ * create a new service (through Admin API) and to send
+ * a new message (throught Digital Citizenship API).
+ *
+ * Once created, the API-Key for the user subscription will be written 
+ * to standard output.
+ *
+ */
+import { login } from "../../lib/login";
+
+import { IResourcesConfiguration, readConfig } from "../../lib/config";
+
+import { IUserData, updateApimUser } from "../account";
+import { createService } from "../service";
+
+import apiManagementClient = require("azure-arm-apimanagement");
+
+const createAdminUser = async (configx, apiClient) => {
+  const userData: IUserData = {
+    email: "apim-deploy@agid.gov.it",
+    firstName: "azure",
+    groups: [
+      "ApiProfileWrite",
+      "ApiMessageWrite",
+      "ApiMessageRead",
+      "ApiServiceWrite",
+      "ApiServiceRead"
+    ],
+    lastName: "deploy",
+    oid: "azure-deploy",
+    productName: config.apimProductName
+  };
+
+  const user = await apiClient.user.createOrUpdate(
+    config.azurermResourceGroup,
+    config.azurermApim,
+    userData.oid,
+    {
+      email: userData.email,
+      firstName: userData.firstName,
+      lastName: userData.lastName
+    }
+  );
+
+  if (!user || !user.name) {
+    throw new Error("Cannot create new user");
+  }
+
+  const subscription = await updateApimUser(user.name, userData, creds, true);
+
+  if (!subscription || !subscription.name) {
+    throw new Error("Cannot create subscription");
+  }
+
+  await createService(subscription.primaryKey, {
+    authorized_cidrs: [],
+    authorized_recipients: [],
+    department_name: "IT",
+    organization_name: "AgID",
+    service_id: subscription.name,
+    service_name: "Digital Citizenship"
+  });
+
+  return subscription.primaryKey;
+};
+
+export const run = async (config: IResourcesConfiguration) => {
+  const loginCreds = await login();
+
+  const apiClient = new apiManagementClient(
+    loginCreds.creds as any,
+    loginCreds.subscriptionId
+  );
+
+  await createAdminUser(config, apiClient);
+};
+
+readConfig(process.env.ENVIRONMENT)
+  .then(run)
+  .then(r => {
+    if (r) {
+      console.log("Successfully set up developer portal API_KEY");
+    }
+  })
+  .catch((e: Error) => console.error(process.env.VERBOSE ? e : e.message));
+
+// createAdminUser(localConfig)
+//   .then(key => console.log("set ADMIN_API_KEY=" + key))
+//   .catch(console.error);
---
+++ infrastructure/tasks/31-devapp_setup.ts
@@ -0,0 +1,63 @@
+/**
+ * Run this task from the command line to set up Application Settings
+ * for the developer portal facilities web application.
+ * 
+ * https://github.com/teamdigitale/digital-citizenship-onboarding
+ * 
+ * Two further steps are needed to complete the Web App deployment: 
+ *  - activate MSI for the application
+ *  - register the application as "Contributor" into the API management IAM 
+ */
+// tslint:disable:no-console
+// tslint:disable:no-any
+
+import { login } from "../../lib/login";
+
+import { IResourcesConfiguration, readConfig } from "../../lib/config";
+
+import webSiteManagementClient = require("azure-arm-website");
+
+const CLIENT_ID = process.env.DEV_PORTAL_EXT_CLIENT_ID;
+const CLIENT_SECRET = process.env.DEV_PORTAL_EXT_CLIENT_SECRET;
+const ARM_SUBSCRIPTION_ID = process.env.ARM_SUBSCRIPTION_ID;
+
+export const run = async (config: IResourcesConfiguration) => {
+  const loginCreds = await login();
+
+  const webSiteClient = new webSiteManagementClient(
+    loginCreds.creds as any,
+    loginCreds.subscriptionId
+  );
+
+  const conf = await webSiteClient.webApps.listApplicationSettings(
+    config.azurerm_resource_group,
+    config.azurerm_app_service_portal
+  );
+
+  // Set up environment variables in application settings
+  if (conf.properties) {
+    return webSiteClient.webApps.updateApplicationSettings(
+      config.azurerm_resource_group,
+      config.azurerm_app_service_portal,
+      {
+        properties: {
+          ...conf.properties,
+          ARM_SUBSCRIPTION_ID,
+          CLIENT_ID,
+          CLIENT_SECRET
+        }
+      }
+    );
+  }
+
+  return Promise.reject(new Error("Could not retrieve Application Settings"));
+};
+
+readConfig(process.env.ENVIRONMENT)
+  .then(run)
+  .then(r => {
+    if (r) {
+      console.log("Successfully updated developer portal application settings");
+    }
+  })
+  .catch((e: Error) => console.error(process.env.VERBOSE ? e : e.message));
